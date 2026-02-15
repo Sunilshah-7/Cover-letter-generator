a13d73133824855a37901129561a4c5115c3b972
Sunil Shah <sunilshah2416@gmail.com>
2026-02-14 23:26:11 -0500

    removing worker file from next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      config.resolve.alias.canvas = false;
-    }
-    return config;
-  },
-  // Ensure API routes are included in the build
-  experimental: {
-    serverComponentsExternalPackages: ["pdfjs-dist"],
-  },
+  /* config options here */
 };
 
 export default nextConfig;
